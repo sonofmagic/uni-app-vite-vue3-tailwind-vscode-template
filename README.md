01c372c9b13350fe86417119820edc3f19b2f1d5
SonOfMagic <qq1324318532@gmail.com>
2022-02-21 00:01:31 +0800

    feat: commit all assets

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# uni-app-vite-vue3-tailwind-vscode-template-
-uni-app-vite-vue3-tailwind-vscode-template 
+# uni-app-vite-vue3-tailwind-vscode-template
+
+基于 uni-app 的 vite/vue3 tailwindcss 模板
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import { ViteWeappTailwindcssPlugin } from 'weapp-tailwindcss-webpack-plugin'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni(), ViteWeappTailwindcssPlugin()],
+  css: {
+    postcss: {
+      plugins: [
+        require('autoprefixer')(),
+        require('tailwindcss')(),
+        require('postcss-rem-to-responsive-pixel')({
+          rootValue: 32,
+          propList: ['*'],
+          transformUnit: 'rpx'
+        })
+      ]
+    }
+  }
+})
