82b17af0801836a12f0539721d7cd09417fb7441
SonOfMagic <qq1324318532@gmail.com>
2022-02-23 09:26:58 +0800

    feat: commit latest change

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
-import { ViteWeappTailwindcssPlugin } from 'weapp-tailwindcss-webpack-plugin'
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+import { ViteWeappTailwindcssPlugin as vwt } from "weapp-tailwindcss-webpack-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni(), ViteWeappTailwindcssPlugin()],
-  css: {
-    postcss: {
-      plugins: [
-        require('autoprefixer')(),
-        require('tailwindcss')(),
-        require('postcss-rem-to-responsive-pixel')({
-          rootValue: 32,
-          propList: ['*'],
-          transformUnit: 'rpx'
-        })
-      ]
-    }
-  }
-})
+  plugins: [uni(), vwt()],
+});
