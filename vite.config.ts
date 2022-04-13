15225233095d952f4d7ff8323e24ed7a82e4e791
SonOfMagic <qq1324318532@gmail.com>
2022-04-13 17:44:40 +0800

    fix: latest uni-app commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,25 @@ import {
   ViteWeappTailwindcssPlugin as vwt,
   postcssWeappTailwindcssRename,
 } from "weapp-tailwindcss-webpack-plugin";
+
+const postcssPlugins = [require("autoprefixer")(), require("tailwindcss")()];
+if (!isH5) {
+  postcssPlugins.push(
+    require("postcss-rem-to-responsive-pixel")({
+      rootValue: 32,
+      propList: ["*"],
+      transformUnit: "rpx",
+    })
+  );
+  postcssPlugins.push(postcssWeappTailwindcssRename());
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni(), isH5 ? undefined : vwt()],
   // 假如 postcss.config.js 不起作用，请使用内联 postcss
   css: {
     postcss: {
-      plugins: [
-        require("autoprefixer")(),
-        require("tailwindcss")(),
-        require("postcss-rem-to-responsive-pixel")({
-          rootValue: 32,
-          propList: ["*"],
-          transformUnit: "rpx",
-        }),
-        postcssWeappTailwindcssRename(),
-      ],
+      plugins: postcssPlugins,
     },
   },
 });
