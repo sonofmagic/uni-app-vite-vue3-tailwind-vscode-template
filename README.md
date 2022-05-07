b8335e14078aab1fd93a7e8d70607df4cce2f390
SonOfMagic <1324318532@qq.com>
2022-05-07 19:02:25 +0800

    feat: commit cjs and map demo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 基于 uni-app 的 vite/vue3 tailwindcss 模板
 
 > `postcss.config.js` 假如不起作用，请使用 `内联postcss`
+
+## Tips
+
+`vite` 使用高德地图加载 `commonjs` 模块示例见 https://github.com/sonofmagic/uni-app-vue3-for-amap-wx
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 const isH5 = process.env.UNI_PLATFORM === "h5";
+// 假如要加载一些 commonjs 模块，需要引入这个插件，很多地图的sdk都是 commonjs，假如引用报错需要引入它并添加到 `plugins` 里
+// import commonjs from "@rollup/plugin-commonjs";
 import {
   ViteWeappTailwindcssPlugin as vwt,
   postcssWeappTailwindcssRename,
