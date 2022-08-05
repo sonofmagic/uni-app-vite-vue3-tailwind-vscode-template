87ee4c2b55697000faf41216fb343c8d29b0ab7c
SonOfMagic <qq1324318532@gmail.com>
2022-08-05 15:33:33 +0800

    feat: compat h5 and app

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -70,17 +70,19 @@
 import { ref, onBeforeUnmount } from "vue";
 const title = ref("Hello");
 const themeRef = ref(uni.getSystemInfoSync().theme);
-
+// #ifdef MP
 uni.onThemeChange(({ theme }: { theme: "dark" | "light" }) => {
   themeRef.value = theme;
 });
-
+// #endif
 onBeforeUnmount(() => {
+  // #ifdef MP
   uni.offThemeChange(() => {});
+  // #endif
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .test {
   @apply flex items-center justify-center h-[100px] w-[100px] rounded-[40px] bg-[#123456] bg-opacity-[0.54] text-[#ffffff] #{!important};
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 const isH5 = process.env.UNI_PLATFORM === "h5";
+const isApp = process.env.UNI_PLATFORM === "app";
+const WeappTailwindcssDisabled = isH5 || isApp;
 // 假如要加载一些 commonjs 模块，需要引入这个插件，很多地图的sdk都是 commonjs，假如引用报错需要引入它并添加到 `plugins` 里
 // import commonjs from "@rollup/plugin-commonjs";
 import vwt from "weapp-tailwindcss-webpack-plugin/vite";
 import postcssWeappTailwindcssRename from "weapp-tailwindcss-webpack-plugin/postcss";
 
 const postcssPlugins = [require("autoprefixer")(), require("tailwindcss")()];
-if (!isH5) {
+if (!WeappTailwindcssDisabled) {
   postcssPlugins.push(
     require("postcss-rem-to-responsive-pixel")({
       rootValue: 32,
@@ -19,7 +21,7 @@ if (!isH5) {
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni(), isH5 ? undefined : vwt()],
+  plugins: [uni(), WeappTailwindcssDisabled ? undefined : vwt()],
   // 假如 postcss.config.js 不起作用，请使用内联 postcss
   css: {
     postcss: {
