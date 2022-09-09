f375e570381f7b3b14a2e67197047e47be917f3d
sonofmagic <1324318532@qq.com>
2022-09-10 01:59:09 +0800

    chore: no need to import weapp-tailwindcss-webpack-plugin/postcss

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ const WeappTailwindcssDisabled = isH5 || isApp;
 // 假如要加载一些 commonjs 模块，需要引入这个插件，很多地图的sdk都是 commonjs，假如引用报错需要引入它并添加到 `plugins` 里
 // import commonjs from "@rollup/plugin-commonjs";
 import vwt from "weapp-tailwindcss-webpack-plugin/vite";
-import postcssWeappTailwindcssRename from "weapp-tailwindcss-webpack-plugin/postcss";
 
 const postcssPlugins = [require("autoprefixer")(), require("tailwindcss")()];
 if (!WeappTailwindcssDisabled) {
@@ -17,10 +16,10 @@ if (!WeappTailwindcssDisabled) {
       transformUnit: "rpx",
     })
   );
-  postcssPlugins.push(postcssWeappTailwindcssRename());
 }
 // https://vitejs.dev/config/
 export default defineConfig({
+  // vwt 一定要放在 uni 后面
   plugins: [uni(), WeappTailwindcssDisabled ? undefined : vwt()],
   // 假如 postcss.config.js 不起作用，请使用内联 postcss
   css: {
