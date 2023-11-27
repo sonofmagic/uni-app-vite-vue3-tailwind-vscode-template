75ec7634b8ba2369e4a3e891fda3d803688867d4
ice breaker <1324318532@qq.com>
2023-11-27 16:34:05 +0800

    chore: upgrade weapp-tailwindcss to 3.x

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,44 +1,37 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { WeappTailwindcssDisabled } = require('./platform')
+// const { WeappTailwindcssDisabled } = require('./platform')
 
+/**
+ * @type {import('postcss').AcceptedPlugin[]}
+ */
 const plugins = [require('tailwindcss')(), require('autoprefixer')()]
 
-if (!WeappTailwindcssDisabled) {
-  plugins.push(
-    require('postcss-rem-to-responsive-pixel')({
-      rootValue: 32,
-      propList: ['*'],
-      transformUnit: 'rpx'
-    })
-  )
-  // px 转 rpx 功能
-  // plugins.push(
-  //   require('postcss-pxtransform')({
-  //     platform: 'weapp',
-  //     // https://taro-docs.jd.com/docs/size
-  //     // 根据你的设计稿宽度进行配置
-  //     // 可以传入一个 function
-  //     // designWidth (input) {
-  //     //   if (input.file.replace(/\\+/g, '/').indexOf('@nutui/nutui-taro') > -1) {
-  //     //     return 375
-  //     //   }
-  //     //   return 750
-  //     // },
-  //     designWidth: 750, // 375,
-
-  //     deviceRatio: {
-  //       640: 2.34 / 2,
-  //       // 此时应用到的规则，代表 1px = 1rpx
-  //       750: 1,
-  //       828: 1.81 / 2,
-  //       // 假如你把 designWidth 设置成 375 则使用这条规则 1px = 2rpx
-  //       375: 2 / 1
-  //     }
-  //   })
-  // )
-}
+// 下方为 px 转 rpx 功能
+// if (!WeappTailwindcssDisabled) {
+// plugins.push(
+//   require('postcss-pxtransform')({
+//     platform: 'weapp',
+//     // https://taro-docs.jd.com/docs/size
+//     // 根据你的设计稿宽度进行配置
+//     // 可以传入一个 function
+//     // designWidth (input) {
+//     //   if (input.file.replace(/\\+/g, '/').indexOf('@nutui/nutui-taro') > -1) {
+//     //     return 375
+//     //   }
+//     //   return 750
+//     // },
+//     designWidth: 750, // 375,
+//     deviceRatio: {
+//       640: 2.34 / 2,
+//       // 此时应用到的规则，代表 1px = 1rpx
+//       750: 1,
+//       828: 1.81 / 2,
+//       // 假如你把 designWidth 设置成 375 则使用这条规则 1px = 2rpx
+//       375: 2 / 1
+//     }
+//   })
+// )
+// }
 plugins.push(require('weapp-tailwindcss/css-macro/postcss'))
 
-module.exports = {
-  plugins
-}
+module.exports = plugins
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
-// import Components from 'unplugin-vue-components/vite'
-// 假如要加载一些 commonjs 模块，需要引入这个插件，很多地图的sdk都是 commonjs，假如引用报错需要引入它并添加到 `plugins` 里
-// import commonjs from "@rollup/plugin-commonjs";
 import { UnifiedViteWeappTailwindcssPlugin as uvtw } from 'weapp-tailwindcss/vite'
 import { WeappTailwindcssDisabled } from './platform'
-import { plugins as postcssPlugins } from './postcss.config.cjs'
+import postcssPlugins from './postcss.config.cjs'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // uvtw 一定要放在 uni 后面
   plugins: [
     uni(),
     uvtw({
+      rem2rpx: true,
       disabled: WeappTailwindcssDisabled
     }),
     AutoImport({
@@ -22,10 +21,6 @@ export default defineConfig({
         enabled: true
       }
     })
-    // uni-app vite 中不起作用，不知道为啥
-    // Components({
-    //   dts: './src/components.d.ts'
-    // })
   ],
   // 内联 postcss 注册 tailwindcss
   css: {
