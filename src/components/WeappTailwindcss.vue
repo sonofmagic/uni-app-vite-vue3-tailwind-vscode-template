f06e6326322759effb62a353038e2e1761728fbb
ice breaker <1324318532@qq.com>
2024-09-07 21:54:42 +0800

    chore(deps): upgrade

--- src/components/WeappTailwindcss.vue
+++ src/components/WeappTailwindcss.vue
@@ -30,11 +30,11 @@ function copy(data) {
     <view class="text-xs text-gray-600/75">
       你可以在上方的官网上，找到大量的提效解决方案
     </view>
-
+    <view class="text-xs text-gray-600/75">
+      在使用这个模板之前，确保你安装了 Nodejs@20 和 Vscode，然后安装了 eslint 插件，本模板使用 eslint 进行错误校验和格式化
+    </view>
     <view
       class="mb-2 text-center before:text-xs before:text-sky-400 before:content-['现在，让我们开始神奇的_tailwindcss_开发之旅吧！']"
     />
   </view>
 </template>
-
-<style lang="scss" scoped></style>
