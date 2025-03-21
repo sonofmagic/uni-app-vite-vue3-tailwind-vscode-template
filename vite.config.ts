059d78711af56f76f7099bd06d459c5cccefbf52
ice breaker <1324318532@qq.com>
2025-03-21 16:24:02 +0800

    chore(deps): upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
-import { UnifiedViteWeappTailwindcssPlugin as uvtw } from 'weapp-tailwindcss/vite'
+import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
 import { WeappTailwindcssDisabled } from './platform'
 import postcssPlugins from './postcss.config'
 
@@ -10,12 +10,9 @@ export default defineConfig({
   // uvtw 一定要放在 uni 后面
   plugins: [
     uni(),
-    uvtw({
+    UnifiedViteWeappTailwindcssPlugin({
       rem2rpx: true,
       disabled: WeappTailwindcssDisabled,
-      // 使用新的 ast-grep 来处理 js 资源，速度是 babel 的2倍左右
-      // 需要先安装 `@ast-grep/napi`, 安装完成后再启用下方配置
-      // jsAstTool: 'ast-grep'
     }),
     AutoImport({
       imports: ['vue', 'uni-app', 'pinia'],
