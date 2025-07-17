19b4269e5d7462078e7d19e1259cdc33b0b1a653
ice breaker <1324318532@qq.com>
2025-07-17 16:47:32 +0800

    chore: upgrade uni-app and deps

--- postcss.config.ts
+++ postcss.config.ts
@@ -6,6 +6,7 @@ import cssMacro from 'weapp-tailwindcss/css-macro/postcss'
 const plugins: AcceptedPlugin[] = [tailwindcss(), autoprefixer()]
 
 // 可以使用 postcss-pxtransform 来进行 px 转 rpx 的功能
+// 详见: https://tw.icebreaker.top/docs/quick-start/css-unit-transform#px-%E8%BD%AC-rpx
 
 plugins.push(cssMacro)
 
