aecc4d836a21bc66d03eeae7f5dfa277ba0cbc5b
ice breaker <1324318532@qq.com>
2026-05-23 23:16:34 +0800

    chore(deps): upgrade

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,8 @@
 import type { AcceptedPlugin } from 'postcss'
+import autoprefixer from 'autoprefixer'
+
 import cssMacro from 'weapp-tailwindcss/css-macro/postcss'
 
-const plugins: AcceptedPlugin[] = [cssMacro]
+const plugins: AcceptedPlugin[] = [autoprefixer(), cssMacro]
 
 export default plugins
